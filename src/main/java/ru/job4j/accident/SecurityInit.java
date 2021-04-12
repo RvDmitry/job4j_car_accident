82a5c0da5f2203ca46991e0f8d659be85582be09
RvDmitry <rvdmitry@gmail.com>
2021-04-13 00:53:35 +0600

    0. Spring Security [#407117]

---
+++ src/main/java/ru/job4j/accident/SecurityInit.java
@@ -0,0 +1,12 @@
+package ru.job4j.accident;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Class SecurityInit
+ *
+ * @author Dmitry Razumov
+ * @version 1
+ */
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
